eafda1bf160e4f70b0c2a532e733714515c72f4d
Iván Vázquez Martín <ivanvmartin@gmail.com>
2025-12-11 18:15:21 +0100

    Commit inicial

---
+++ src/main/java/programa_cliente/Cliente.java
@@ -0,0 +1,29 @@
+package programa_cliente;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+
+public class Cliente {
+
+	public static void main(String[] args) {
+		String url = "localhost:8080/prueba";
+		String contenido = "";
+		HttpClient cliente = HttpClient.newHttpClient();
+		HttpRequest peticion;
+		HttpResponse<String> respuesta = null;
+		try {
+			peticion = HttpRequest.newBuilder().uri(URI.create(url)).build();
+			respuesta = cliente.send(peticion, BodyHandlers.ofString());
+			contenido = respuesta.body();
+			System.out.println(contenido);
+		} catch (IOException | InterruptedException e) {
+			e.printStackTrace();
+			System.exit(-1);
+		}
+	}
+
+}
